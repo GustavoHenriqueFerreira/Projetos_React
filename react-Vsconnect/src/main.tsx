6ce5995bf171bac321fef518af768484d5f8a578
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2023-08-08 20:16:08 -0300

    feat: Página de Lista se serviços adicionada

--- react-Vsconnect/src/main.tsx
+++ react-Vsconnect/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Home from './pages/Home/index.tsx';
+import ListaServicos from './pages/ListaServicos/index.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+   <Router>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/listaServicos' element={<ListaServicos/>} /> 
+        </Routes>                   
+      </Router>
   </React.StrictMode>,
 )
